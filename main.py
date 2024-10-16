c034151a0934092eedb20d256772890530dcc9ae
Anderson Lima <67929816+A-Infor@users.noreply.github.com>
2024-10-16 15:33:24 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,63 +1,9 @@
 import pandas as pd
 import numpy as np
 
-# Lista de cidades para procurar
-cidades_a_procurar = [
-    'Capitão Enéas',
-    'Ibiracatu',
-    'Janaúba',
-    'Japonvar',
-    'Lontra',
-    'Montes Claros',
-    'Patis',
-    'Varzelândia',
-    'Verdelândia',
-    'São João da Ponte'
-]
-cidades_a_procurar = [cidade.upper() for cidade in cidades_a_procurar]
-
-# Abrir o arquivo Excel com a segunda coluna a ser concatenada
-DF_DATAS = pd.read_excel('São João da Ponte_revisado_final.xlsx')
-
-# Filtrar para apenas os municipios de Minas Gerais (codigos iniciados com 31)
-# Minas Gerais tem 853 municipios.
-city_coordinates = pd.read_excel('CoordenadasMunicipios.xlsx', index_col='GEOCODIGO_MUNICIPIO')
-city_coordinates = city_coordinates[ city_coordinates.index.astype(str).str.startswith('31') ].set_index('NOME_MUNICIPIO')
-
-# Criar um dicionário com os nomes dos municípios como chaves e suas coordenadas como valores
-# Encontrar as coordenadas das cidades procuradas e deixar os nomes em caixa alta
-found_cities = {}
-for sought_city in cidades_a_procurar:
-    found_cities[sought_city] = {
-        "longitude" : float(city_coordinates.loc[sought_city]['LONGITUDE']),
-        "latitude"  : float(city_coordinates.loc[sought_city]['LATITUDE'])
-    }
-
-# Exibir os resultados
-for cidade, coords in found_cities.items():
-    print(f'{cidade}: {coords}')
-    
-# Abrir o arquivo CSV
-dfSpei = pd.read_csv("speiAll_final.csv",delimiter=';')
-
-# Remover as primeiras 11 linhas
-dfSpei = dfSpei.iloc[11:].reset_index(drop=True)
-
-print(dfSpei)
-
-# Função para converter as coordenadas em valores negativos
-def convert_to_negative(coord):
-    # Divide a string em partes
-    parts = coord.split(',')
-    # Pega a latitude e longitude
-    latitude = '-'+parts[1] + '.' + parts[2]
-    longitude = '-'+parts[4] + '.' + parts[5]
-    # Converte para negativos e retorna no formato original
-    return f'X,{float(latitude) },{float(longitude) }'
-
-# Função para calcular a distância Euclidiana entre duas coordenadas
-def euclidean_distance(coord1, coord2):
-    return np.sqrt((coord1[0] - coord2[0])**2 + (coord1[1] - coord2[1])**2)
+def find_cities_coordinates(cidades_a_procurar, DF_COORDS):
+    """
+    Return the cities' coordinates given their names.
 
     Parameters
     ----------
@@ -71,48 +17,54 @@ def euclidean_distance(coord1, coord2):
                             The coordinates, in turn, are a subdictionary composed of 'longitude' and 'latitude' as keys, and the numbers as values.
 
     """
-    DF_COORDS = pd.read_excel(DF_COORDS, usecols=['NOME_MUNICIPIO', 'LONGITUDE', 'LATITUDE'])
+    cidades_a_procurar = [cidade.upper() for cidade in cidades_a_procurar]
     
-    MUNICIPIOS_DICT = {
-        row['NOME_MUNICIPIO']: {
-            'longitude': round(row['LONGITUDE'], 2), 
-            'latitude': round(row['LATITUDE'], 2)
+    # Filtrar para apenas os municipios de Minas Gerais (codigos iniciados com 31)
+    # Minas Gerais tem 853 municipios.
+    city_coordinates = pd.read_excel(DF_COORDS, index_col='GEOCODIGO_MUNICIPIO')
+    city_coordinates = city_coordinates[ city_coordinates.index.astype(str).str.startswith('31') ].set_index('NOME_MUNICIPIO')
+    
+    found_cities = {}
+    for sought_city in cidades_a_procurar:
+        found_cities[sought_city] = {
+            "longitude" : float(city_coordinates.loc[sought_city]['LONGITUDE']),
+            "latitude"  : float(city_coordinates.loc[sought_city]['LATITUDE'])
         }
-        for _, row in DF_COORDS.iterrows()
-    }
-   
-    # Encontrar as coordenadas das cidades procuradas e deixar os nomes em caixa alta
-    CITIES_COORDINATES_DICT = {cidade.upper(): MUNICIPIOS_DICT.get(cidade.upper(), 'Cidade não encontrada') for cidade in CIDADES_A_PROCURAR}
-    
+        
     # Exibir os resultados
-    for cidade, coords in CITIES_COORDINATES_DICT.items():
+    for cidade, coords in found_cities.items():
         print(f'{cidade}: {coords}')    
+    print()
+    
+    return found_cities
 
-    return CITIES_COORDINATES_DICT
+def find_nearest_gauging_station(coords, dfSpei):
+    """
+    Return the coordinates for the nearest gauging station, given the coordinates of the city of interest.
 
-# Exibir os resultados
-for cidade, coords in RESULTADOS.items():
-    print(f'{cidade}: {coords}')
-     
-# Abrir o arquivo CSV, remover as 11 primeiras linhas, e converter todos os nomes de colunas para números negativos
-dfSpei = pd.read_csv("speiAll_final.csv",delimiter=';').iloc[11:].reset_index(drop=True)
-print(dfSpei)
-dfSpei.columns = [convert_coordinates_to_negative(col) for col in dfSpei.columns]
+    Parameters
+    ----------
+    coords : the coordinates of the city of interest.
+    dfSpei : the dataframe in which to do the search. It has the coordinates of every gauging station as column names.
 
-# Armazenar os resultados
-result_dict = {}
+    Returns
+    -------
+    closest_col : the coordinates of the gauging station geographically nearest to the city of interest.
 
-def find_nearest_city(municipio, coords, dfSpei):
+    """
+    
     lat_municipio = coords['latitude']
     lon_municipio = coords['longitude']
-
+    
     # Converter colunas para coordenadas
     min_distance = float('inf')
-    closest_col = None
+    closest_col_name = None
 
     for col in dfSpei.columns:
-        # Supondo o formato das coordenadas como 'X,lon,lat'
-        (lon_col, lat_col) = map(float, col.lstrip("X,").split(',') )
+        # Supondo o formato das coordenadas como 'X,lat,lon'
+        parts = col.split(',')
+        lon_col = float(parts[1])
+        lat_col = float(parts[2])
         # Calcular a distância
         coordinates_euclidean_distance = lambda COORD1, COORD2 : np.sqrt((COORD1[0] - COORD2[0])**2 + (COORD1[1] - COORD2[1])**2)
         distance = coordinates_euclidean_distance((lat_municipio, lon_municipio), (lat_col, lon_col))
@@ -120,8 +72,9 @@ def find_nearest_city(municipio, coords, dfSpei):
         # Encontrar a coluna com a menor distância
         if distance < min_distance:
             min_distance = distance
-            closest_col = col
-    return closest_col
+            closest_col_name = col
+
+    return closest_col_name
 
 def save_city_SPEI_on_xlsx(cidade, coluna_proxima, dfSpei, DF_DATAS):
     """
@@ -162,10 +115,27 @@ def save_city_SPEI_on_xlsx(cidade, coluna_proxima, dfSpei, DF_DATAS):
         # Salva em um arquivo Excel
         df_city.to_excel(f'./Data/{cidade}.xlsx', index=False)
         print(f'Salvo {cidade}.xlsx')
+        return True
     else:
         print(f'Coluna para {cidade} não encontrada.')
-  
+        return False
+
 def convert_coordinates_to_negative(COORD):
+    """
+    Return negative coordinates provided any coordinates.
+    For example, 'X,47,95,,19,55' will be converted to 'X,-47.95,-19.55'
+
+    Parameters
+    ----------
+    COORD : string
+        Any coordinates.
+
+    Returns
+    -------
+    str
+        The negative coordinates.
+
+    """
     # Divide a string em partes
     PARTS = COORD.split(',')
     # Pega a latitude e longitude
@@ -173,27 +143,26 @@ def convert_coordinates_to_negative(COORD):
     LONGITUDE = '-' + PARTS[4] + '.' + PARTS[5]
     # Converte para negativos e retorna no formato original
     return f'X,{float(LATITUDE) },{float(LONGITUDE) }'
-
-def coordinates_euclidean_distance(COORD1, COORD2):
-    return np.sqrt((COORD1[0] - COORD2[0])**2 + (COORD1[1] - COORD2[1])**2)
-
+    
+CIDADES_PETERSON = [
+    'Espinosa',
+    'Rio Pardo de Minas',
+    'São Francisco',
+    'São João da Ponte',
+    'Lassance'
+]
 
 # Abrir o arquivo Excel com a segunda coluna a ser concatenada
 DF_DATAS = pd.read_excel('São João da Ponte_revisado_final.xlsx')
-    
-# Abrir o arquivo CSV
-dfSpei = pd.read_csv("speiAll_final.csv",delimiter=';')
 
-# Remover as primeiras 11 linhas
-dfSpei = dfSpei.iloc[11:].reset_index(drop=True)
+# Abrir o arquivo CSV, remover as 11 primeiras linhas, e converter todos os nomes de colunas para números negativos
+dfSpei = pd.read_csv("speiAll_final.csv",delimiter=';').iloc[11:].reset_index(drop=True)
 print(dfSpei)
-
-# Aplicar função em todas as colunas
 dfSpei.columns = [convert_coordinates_to_negative(col) for col in dfSpei.columns]
 
-result_dict = {}   
-for municipio, coords in find_cities_coordinates().items():
-    result_dict[municipio] = find_nearest_city(municipio, coords, dfSpei)
+nearest_coordinates_dict = {}   
+for municipio, coords in find_cities_coordinates(CIDADES_PETERSON, 'CoordenadasMunicipios.xlsx').items():
+    nearest_coordinates_dict[municipio] = find_nearest_gauging_station(coords, dfSpei)
 
-for cidade, coluna_proxima in result_dict.items():
+for cidade, coluna_proxima in nearest_coordinates_dict.items():
     save_city_SPEI_on_xlsx(cidade, coluna_proxima, dfSpei, DF_DATAS)
