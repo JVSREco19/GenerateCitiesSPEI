5bc4e35b02891b4f286a1bbc48f0d92e208e15fc
Anderson Lima <67929816+A-Infor@users.noreply.github.com>
2024-10-16 15:42:46 -0300

    `main.py`: `find_cities_coordinates` → `find_Minas_Gerais_cities_coordinates`
    I rewrote the function `find_cities_coordinates` into `find_Minas_Gerais_cities_coordinates`.
    
    Benefit: it uses only one dataframe from start to end, narrowing it down step-by-step, instead of extracting data from it and inserting into a dictionary, just to extract again and insert into another dictionary. This was clumsy.
    
    As a result, `find_Minas_Gerais_cities_coordinates` returns a dataframe, not a dictionary. The function that takes its output as input, `find_nearest_gauging_station`, was not yet rewritten to take the dataframe as an input directly. As such, a workaround is used to feed `find_nearest_gauging_station` with something that looks like a dictionary, as it expects to.
    
    The function `find_nearest_gauging_station` must be rewritten in the near future to take advantage of the dataframe returned by `find_Minas_Gerais_cities_coordinates`, in order to eliminate the dictionary-like workaround quoted in the next lines:
        
        # Create a dictionary to hold the coordinates of the nearest gauging station in relation to the sought city:
        nearest_coordinates_dict = {}   
        for city in df_cities_coordinates.index:
            nearest_coordinates_dict[city] = find_nearest_gauging_station({'longitude': df_cities_coordinates.loc[city]['LONGITUDE'], 'latitude': df_cities_coordinates.loc[city]['LATITUDE']}, df_SPEI)

--- main.py
+++ main.py
@@ -1,44 +1,71 @@
 import pandas as pd
 import numpy as np
 
-def find_cities_coordinates(cidades_a_procurar, DF_COORDS):
+import os
+os.chdir('E:\POSCOMP\Python\GenerateCitiesSPEI-local-paráfrase')
+
+CIDADES_PETERSON = [
+    'Espinosa',
+    'Rio Pardo de Minas',
+    'São Francisco',
+    'São João da Ponte',
+    'Lassance'
+]
+
+def convert_coordinates_to_negative(COORD):
     """
-    Return the cities' coordinates given their names.
+    Return negative coordinates provided any coordinates.
+    For example, 'X,47,95,,19,55' will be converted to 'X,-47.95,-19.55'
 
     Parameters
     ----------
-    CIDADES_A_PROCURAR : list of city names to search for on DF_COORDS.
-    DF_COORDS          : a xlsx file that has info about all Minas Gerais cities -- name, longitude and latitude.
+    COORD : string
+        Any coordinates.
 
     Returns
     -------
-    CITIES_COORDINATES_DICT : dictionary of cities and their corresponding coordinates (longitude and latitude).
-                            The city names are the keys, and their coordinates are the values.
-                            The coordinates, in turn, are a subdictionary composed of 'longitude' and 'latitude' as keys, and the numbers as values.
+    str
+        The negative coordinates.
 
     """
-    cidades_a_procurar = [cidade.upper() for cidade in cidades_a_procurar]
-    
-    # Filtrar para apenas os municipios de Minas Gerais (codigos iniciados com 31)
-    # Minas Gerais tem 853 municipios.
-    city_coordinates = pd.read_excel(DF_COORDS, index_col='GEOCODIGO_MUNICIPIO')
-    city_coordinates = city_coordinates[ city_coordinates.index.astype(str).str.startswith('31') ].set_index('NOME_MUNICIPIO')
-    
-    found_cities = {}
-    for sought_city in cidades_a_procurar:
-        found_cities[sought_city] = {
-            "longitude" : float(city_coordinates.loc[sought_city]['LONGITUDE']),
-            "latitude"  : float(city_coordinates.loc[sought_city]['LATITUDE'])
-        }
-        
-    # Exibir os resultados
-    for cidade, coords in found_cities.items():
-        print(f'{cidade}: {coords}')    
-    print()
-    
-    return found_cities
+    # Divide a string em partes
+    PARTS = COORD.split(',')
+    # Pega a latitude e longitude
+    LATITUDE  = '-' + PARTS[1] + '.' + PARTS[2]
+    LONGITUDE = '-' + PARTS[4] + '.' + PARTS[5]
+    # Converte para negativos e retorna no formato original
+    return f'X,{float(LATITUDE) },{float(LONGITUDE) }'
 
-def find_nearest_gauging_station(coords, dfSpei):
+def find_Minas_Gerais_cities_coordinates(list_of_cities_to_search_for, cities_coordinates_spreadsheet_name):
+    """
+    Return geographical coordinates given a list of city names.
+
+    Parameters
+    ----------
+    list_of_cities_to_search_for : list
+        The list of cities to search for.
+    cities_coordinates_spreadsheet_name : string
+        The name of the spreadsheet containing all Brazillian city coordinates.
+
+    Returns
+    -------
+    df_cities_coordinates : dataframe
+        A dataframe composed of just the sought city names and their corresponding coordinates.
+        The city names are index labels (NOME_MUNICIPIO) and the LONGITUDE and LATITUDE are the column names.
+
+    """
+    # Uppercase all city names:
+    list_of_cities_to_search_for = [city.upper() for city in list_of_cities_to_search_for]
+    # Creates the dataframe by reading the spreadsheet:
+    df_cities_coordinates = pd.read_excel(cities_coordinates_spreadsheet_name, index_col='GEOCODIGO_MUNICIPIO')
+    # Narrow down the dataframe by keeping only Minas Gerais cities:
+    df_cities_coordinates = df_cities_coordinates[ df_cities_coordinates.index.astype(str).str.startswith('31') ].set_index('NOME_MUNICIPIO')
+    # Narrow down the dataframe by keeping only the sought Minas Gerais cities:
+    df_cities_coordinates = df_cities_coordinates[df_cities_coordinates.index.isin(list_of_cities_to_search_for)]
+    
+    return df_cities_coordinates
+
+def find_nearest_gauging_station(coords, df_SPEI):
     """
     Return the coordinates for the nearest gauging station, given the coordinates of the city of interest.
 
@@ -60,7 +87,7 @@ def find_nearest_gauging_station(coords, dfSpei):
     min_distance = float('inf')
     closest_col_name = None
 
-    for col in dfSpei.columns:
+    for col in df_SPEI.columns:
         # Supondo o formato das coordenadas como 'X,lat,lon'
         parts = col.split(',')
         lon_col = float(parts[1])
@@ -120,49 +147,23 @@ def save_city_SPEI_on_xlsx(cidade, coluna_proxima, dfSpei, DF_DATAS):
         print(f'Coluna para {cidade} não encontrada.')
         return False
 
-def convert_coordinates_to_negative(COORD):
-    """
-    Return negative coordinates provided any coordinates.
-    For example, 'X,47,95,,19,55' will be converted to 'X,-47.95,-19.55'
-
-    Parameters
-    ----------
-    COORD : string
-        Any coordinates.
-
-    Returns
-    -------
-    str
-        The negative coordinates.
-
-    """
-    # Divide a string em partes
-    PARTS = COORD.split(',')
-    # Pega a latitude e longitude
-    LATITUDE  = '-' + PARTS[1] + '.' + PARTS[2]
-    LONGITUDE = '-' + PARTS[4] + '.' + PARTS[5]
-    # Converte para negativos e retorna no formato original
-    return f'X,{float(LATITUDE) },{float(LONGITUDE) }'
-    
-CIDADES_PETERSON = [
-    'Espinosa',
-    'Rio Pardo de Minas',
-    'São Francisco',
-    'São João da Ponte',
-    'Lassance'
-]
 
 # Abrir o arquivo Excel com a segunda coluna a ser concatenada
 DF_DATAS = pd.read_excel('São João da Ponte_revisado_final.xlsx')
 
-# Abrir o arquivo CSV, remover as 11 primeiras linhas, e converter todos os nomes de colunas para números negativos
-dfSpei = pd.read_csv("speiAll_final.csv",delimiter=';').iloc[11:].reset_index(drop=True)
-print(dfSpei)
-dfSpei.columns = [convert_coordinates_to_negative(col) for col in dfSpei.columns]
+# Create a dataframe to hold all SPEI measurements together with their geographical coordinates:
+df_SPEI = pd.read_csv("speiAll_final.csv",delimiter=';').iloc[11:].reset_index(drop=True)
+print(df_SPEI)
+df_SPEI.columns = [convert_coordinates_to_negative(col) for col in df_SPEI.columns]
 
+# Create a dataframe to hold just the Minas Gerais cities of interest:
+df_cities_coordinates = find_Minas_Gerais_cities_coordinates(CIDADES_PETERSON, 'CoordenadasMunicipios.xlsx')
+print(df_cities_coordinates)
+
+# Create a dictionary to hold the coordinates of the nearest gauging station in relation to the sought city:
 nearest_coordinates_dict = {}   
-for municipio, coords in find_cities_coordinates(CIDADES_PETERSON, 'CoordenadasMunicipios.xlsx').items():
-    nearest_coordinates_dict[municipio] = find_nearest_gauging_station(coords, dfSpei)
-
+for city in df_cities_coordinates.index:
+    nearest_coordinates_dict[city] = find_nearest_gauging_station({'longitude': df_cities_coordinates.loc[city, 'LONGITUDE'], 'latitude': df_cities_coordinates.loc[city, 'LATITUDE']}, df_SPEI)
+    
 for cidade, coluna_proxima in nearest_coordinates_dict.items():
-    save_city_SPEI_on_xlsx(cidade, coluna_proxima, dfSpei, DF_DATAS)
+    save_city_SPEI_on_xlsx(cidade, coluna_proxima, df_SPEI, DF_DATAS)
